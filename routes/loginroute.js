0fd82f450cfe10c6d65561eddf17feaa4ee25a94
Violah Chesang <chesangv21@gmail.com>
2023-04-11 10:57:25 +0300

    Created a /search endpoint that has q querystring q.
    It checks if q is empty then it gives an error and if contains something then it prints it

--- routes/loginroute.js
+++ routes/loginroute.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+const family = ['Paul', 'Violah', 'Chelly', 'Israel', 'Ethan'];
+const allMembers = family.forEach(familyMember => {
+    return(familyMember);        
+});
+
+router.get('/search',(req,res) => {
+   let q = req.query.q;
+   console.log(req.query.q);
+   if(q) {
+        res.send(q);        
+   }else{
+        res.send('Invalid query!');
+   }
+});
 
 router.get('/login-form',(req,res) => {
     res.render('login');
+});
+
+router.post('/login-form',(req,res) => {
+
+    res.send({names:allMembers});
 });
 
 module.exports = router
